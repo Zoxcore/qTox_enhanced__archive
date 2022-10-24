79e48fc041f48b4203fe2a04417af43d5b57d968
zoff99 <zoff99@users.noreply.github.com>
2022-10-24 19:50:59 +0200

    fix CI builds

--- buildscripts/build_libx264_windows.sh
+++ buildscripts/build_libx264_windows.sh
@@ -13,12 +13,28 @@ parse_arch --dep "libx264" --supported "win32 win64" "$@"
 
 "${SCRIPT_DIR}/download/download_libx264.sh"
 
-CFLAGS="-O2 -g0" \
-LDFLAGS="-L${DEP_PREFIX}/lib/" \
+if [[ "$SCRIPT_ARCH" == "win64" ]]; then
+# There is a bug in gcc that breaks avx512 on 64-bit Windows https://gcc.gnu.org/bugzilla/show_bug.cgi?id=54412
+# VPX fails to build due to it.
+# This is a workaround as suggested in https://stackoverflow.com/questions/43152633
+    ARCH_FLAGS="-fno-asynchronous-unwind-tables"
+    CROSS_ARG="${MINGW_ARCH}-w64-mingw32-"
+    TARGET_ARG="--target=x86_64-win64-gcc"
+elif [[ "$SCRIPT_ARCH" == "win32" ]]; then
+    ARCH_FLAGS=""
+    CROSS_ARG="${MINGW_ARCH}-w64-mingw32-"
+    TARGET_ARG="--target=x86-win32-gcc"
+else
+    exit 1
+fi
+
+CFLAGS="-O2 -g0 ${ARCH_FLAGS} ${CROSS_CFLAG}" \
+LDFLAGS="-L${DEP_PREFIX}/lib/ ${CROSS_CPPFLAG}" \
 LIBS="-lgdi32 -lws2_32 -lcrypto -lgdi32" \
-CPPFLAGS="-I${DEP_PREFIX}/include" \
+CPPFLAGS="-I${DEP_PREFIX}/include ${CROSS_CPPFLAG}" \
+CROSS="${CROSS_ARG}" \
 ./configure \
-    "${HOST_OPTION}" \
+    --host="${MINGW_ARCH}-w64-mingw32" \
     "--prefix=${DEP_PREFIX}" \
     --disable-opencl \
     --enable-static \
@@ -26,6 +42,9 @@ CPPFLAGS="-I${DEP_PREFIX}/include" \
     --disable-cli \
     --disable-asm \
     --enable-pic
+
+export CC=""
+export WINDRES=""
 
 make -j "${MAKE_JOBS}"
 make install
