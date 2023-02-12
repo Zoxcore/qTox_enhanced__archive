11dd3639016a0b208ffa75835a95d37aa95c1cbd
zoff99 <zoff99@users.noreply.github.com>
2023-02-12 14:47:51 +0100

    local build with H264 hw acceleration

--- buildscripts/build_toxcore_linux.sh
+++ buildscripts/build_toxcore_linux.sh
@@ -27,12 +27,22 @@ build_toxcore() {
 
     patch -Np1 < "${SCRIPT_DIR}/patches/tc___ftv2_capabilities.patch"
 
-    cmake . \
-        -DCMAKE_C_FLAGS="-fstack-protector-all --param=ssp-buffer-size=1" \
-        -DCMAKE_CXX_FLAGS="-fstack-protector-all --param=ssp-buffer-size=1" \
-        -DBOOTSTRAP_DAEMON=OFF \
-        -DCMAKE_BUILD_TYPE=Release \
-        .
+    if [ "$1""x" != "x" ]; then
+        echo "using custom flags: -D${1}"
+        cmake . \
+            -DCMAKE_C_FLAGS=" -D${1} -fstack-protector-all --param=ssp-buffer-size=1" \
+            -DCMAKE_CXX_FLAGS=" -D${1} -fstack-protector-all --param=ssp-buffer-size=1" \
+            -DBOOTSTRAP_DAEMON=OFF \
+            -DCMAKE_BUILD_TYPE=Release \
+            .
+    else
+        cmake . \
+            -DCMAKE_C_FLAGS="-fstack-protector-all --param=ssp-buffer-size=1" \
+            -DCMAKE_CXX_FLAGS="-fstack-protector-all --param=ssp-buffer-size=1" \
+            -DBOOTSTRAP_DAEMON=OFF \
+            -DCMAKE_BUILD_TYPE=Release \
+            .
+    fi
 
     cmake --build . -- -j$(nproc)
     cmake --build . --target install
@@ -66,6 +76,6 @@ build_toxext_messages() {
     popd >/dev/null
 }
 
-build_toxcore
+build_toxcore "$1"
 build_toxext
 build_toxext_messages
